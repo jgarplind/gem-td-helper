fdc9f9c1f40de1fad560908ba374cd4e296e21f6
Joel <joel.garplind@ffcg.se>
2020-02-01 11:45:54 +0100

    added minimal classy ui config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { c, hover } from "classy-ui";
+const header = c(
+  "background-color-gray-400",
+  hover("background-color-gray-200")
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className={header}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
